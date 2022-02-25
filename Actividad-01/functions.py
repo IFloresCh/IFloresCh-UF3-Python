0e85d9bbeeb5e98daf88c6125ab5fbdb43cb5c0d
IFloresCh <iflores@institumvm.cat>
2022-02-25 15:48:53 +0100

    hola

--- Actividad-01/functions.py
+++ Actividad-01/functions.py
@@ -1,7 +1,21 @@
-MSG_1 = "Introduce una frase: "
+MSG_1 = "Introduce una frase maximo 100 caracteres: "
+MSG_2 = 'Introduce una frase para agregar al archivo anterior. Maximo 100 caracteres'
+
+def file_create_str(fname):
+    file = open(fname, "w")
+    str = input(MSG_1)
+    while len(str) > 10:
+      str = input(MSG_1)
+    file.write(str)
+    print(file.read())
+    file.close()
+
+
 def file_add_st(fname):
-  f = open(fname, "a+")
-  str = input(MSG_1)
-  f.write(str)
-  print(f.read())
-  f.close()
+    file = open(fname, "a")
+    str = input(MSG_2)
+    while len(str) > 10:
+      str = input(MSG_2)
+    file.write(str)
+    print(file.read())
+    file.close()
